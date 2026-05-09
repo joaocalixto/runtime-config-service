02b6abcbc01a875eeeb8332d240926934c9edc42
Bea <negromontebs@gmail.com>
2026-05-09 12:04:09 +0100

    Implemented calculated price

--- src/main/java/service/PricingService.java
+++ src/main/java/service/PricingService.java
@@ -2,16 +2,29 @@ package service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.FeatureFlagFileReader;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class PricingService {
     private static final Logger logger = LoggerFactory.getLogger(PricingService.class);
+    private static Map<String, Boolean> flagsResult = new HashMap<>();
 
     public PricingService() {
         logger.info("Starting PricingService");
+        FeatureFlagFileReader featureFlagFileReader = new FeatureFlagFileReader();
+        this.flagsResult = featureFlagFileReader.readFromClasspath("feature-flags.properties");
     }
 
     public double calculatePrice(double basePrice) {
+        Boolean useDiscount = flagsResult.get("use-discount");
+        if (useDiscount) {
+            // 20% discount provided
+            basePrice = basePrice * 0.8;
+        }
         logger.info("Calculating price with PricingService");
+        logger.info("Final price: " + basePrice);
         return basePrice;
     }
 }
